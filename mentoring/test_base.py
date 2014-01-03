3fd36070fe57cbf493f5e78759c63f722e973793
Xavier Antoviaque <xavier@antoviaque.org>
2014-01-03 16:11:17 -0500

    Tests - Allow `go_to_page()` to return any DOM element by CSS selector

--- mentoring/test_base.py
+++ mentoring/test_base.py
@@ -28,13 +28,13 @@ class MentoringBaseTest(SeleniumTest):
         header1 = self.browser.find_element_by_css_selector('h1')
         self.assertEqual(header1.text, 'XBlock scenarios')
 
-    def go_to_page(self, page_name):
+    def go_to_page(self, page_name, css_selector='div.mentoring'):
         """
         Navigate to the page `page_name`, as listed on the workbench home
-        Returns the mentoring DOM element on the visited page
+        Returns the DOM element on the visited page located by the `css_selector`
         """
         self.browser.get(self.live_server_url)
         self.browser.find_element_by_link_text(page_name).click()
-        mentoring = self.browser.find_element_by_css_selector('div.mentoring')
+        mentoring = self.browser.find_element_by_css_selector(css_selector)
         return mentoring
 
