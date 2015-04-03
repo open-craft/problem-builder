409214e49b1512abf896e777d8b5a39d213d3e3e
Braden MacDonald <braden@opencraft.com>
2015-04-02 21:40:06 -0700

    Fix bugs in the upgrade script and change it to work w/ mentoring v1 and v2 installed in parallel

--- problem_builder/v1/upgrade.py
+++ problem_builder/v1/upgrade.py
@@ -24,15 +24,16 @@ This file contains a script to help migrate mentoring blocks to the new format w
 optimized for editing in Studio.
 
 To run the script on devstack:
-SERVICE_VARIANT=cms DJANGO_SETTINGS_MODULE="cms.envs.devstack" python -m mentoring.v1.upgrade
+SERVICE_VARIANT=cms DJANGO_SETTINGS_MODULE="cms.envs.devstack" python -m problem_builder.v1.upgrade [course id here]
 """
 import logging
 from lxml import etree
 from mentoring import MentoringBlock
+from problem_builder import MentoringBlock as NewMentoringBlock
 from StringIO import StringIO
 import sys
+import warnings
 from courseware.models import StudentModule
-from xmodule.modulestore.exceptions import DuplicateItemError
 from .studio_xml_utils import studio_update_from_node
 from .xml_changes import convert_xml_v1_to_v2
 
@@ -42,14 +43,18 @@ def upgrade_block(block):
     Given a MentoringBlock "block" with old-style (v1) data in its "xml_content" field, parse
     the XML and re-create the block with new-style (v2) children and settings.
     """
-    assert isinstance(block, MentoringBlock)
+    assert isinstance(block, (MentoringBlock, NewMentoringBlock))
     assert bool(block.xml_content)  # If it's a v1 block it will have xml_content
     store = block.runtime.modulestore
     xml_content_str = block.xml_content
     parser = etree.XMLParser(remove_blank_text=True)
     root = etree.parse(StringIO(xml_content_str), parser=parser).getroot()
     assert root.tag == "mentoring"
-    convert_xml_v1_to_v2(root)
+    with warnings.catch_warnings(record=True) as warnings_caught:
+        warnings.simplefilter("always")
+        convert_xml_v1_to_v2(root)
+        for warning in warnings_caught:
+            print("    ➔ {}".format(str(warning.message)))
 
     # We need some special-case handling to deal with HTML being an XModule and not a pure XBlock:
     try:
@@ -74,50 +79,41 @@ def upgrade_block(block):
     parent = block.get_parent()
     parent_was_published = not store.has_changes(parent)
 
-    # If the block has a url_name attribute that doesn't match Studio's url_name, fix that:
-    delete_on_success = None
-    if "url_name" in root.attrib:
-        url_name = root.attrib.pop("url_name")
-        if block.url_name != url_name:
-            print(" ➔ This block has two conflicting url_name values set. Attempting to fix...")
-            # Fix the url_name by replacing the block with a blank block with the correct url_name
-            parent_children = parent.children
-            old_usage_id = block.location
-            index = parent_children.index(old_usage_id)
-            try:
-                new_block = store.create_item(
-                    user_id=None,
-                    course_key=block.location.course_key,
-                    block_type="mentoring",
-                    block_id=url_name,
-                    fields={"xml_content": xml_content_str},
-                )
-                delete_on_success = block
-                parent_children[index] = new_block.location
-                parent.save()
-                store.update_item(parent, user_id=None)
-                block = new_block
-                print(" ➔ url_name changed to {}".format(url_name))
-                # Now we've fixed the block's url_name but in doing so we've disrupted the student data.
-                # Migrate it now:
-                student_data = StudentModule.objects.filter(module_state_key=old_usage_id)
-                num_entries = student_data.count()
-                if num_entries > 0:
-                    print(" ➔ Migrating {} student records to new url_name".format(num_entries))
-                    student_data.update(module_state_key=new_block.location)
-            except DuplicateItemError:
-                print(
-                    "\n WARNING: The block with url_name '{}' doesn't match "
-                    "the real url_name '{}' and auto repair failed.\n".format(
-                        url_name, block.url_name
-                    )
-                )
+    old_usage_id = block.location
+    if old_usage_id.block_type != "problem-builder":
+        # We need to change the block type from "mentoring" to "problem-builder", which requires
+        # deleting then re-creating the block:
+        store.delete_item(old_usage_id, user_id=None)
+        parent_children = parent.children
+        index = parent_children.index(old_usage_id)
+
+        if "url_name" in root.attrib:
+            url_name = root.attrib.pop("url_name")
+            if unicode(old_usage_id.block_id) != url_name:
+                print(" ➔ This block has two conflicting url_name values!! Using the XML value : {}".format(url_name))
+        block = store.create_item(
+            user_id=None,
+            course_key=old_usage_id.course_key,
+            block_type="problem-builder",
+            block_id=url_name,
+            fields={"xml_content": xml_content_str},
+        )
+        parent_children[index] = block.location
+        parent.save()
+        store.update_item(parent, user_id=None)
+        print(" ➔ problem-builder created: {}".format(url_name))
+
+        # Now we've changed the block's block_type but in doing so we've disrupted the student data.
+        # Migrate it now:
+        student_data = StudentModule.objects.filter(module_state_key=old_usage_id)
+        num_entries = student_data.count()
+        if num_entries > 0:
+            print(" ➔ Migrating {} student records to new block".format(num_entries))
+            student_data.update(module_state_key=block.location)
 
     # Replace block with the new version and the new children:
     studio_update_from_node(block, root)
 
-    if delete_on_success:
-        store.delete_item(delete_on_success.location, user_id=None)
     if parent_was_published:
         store.publish(parent.location, user_id=None)
 
@@ -148,14 +144,45 @@ if __name__ == '__main__':
     blocks_found = []
 
     def find_mentoring_blocks(block):
-        if isinstance(block, MentoringBlock) and block.xml_content:  # If it's a v1 block it will have xml_content
+        """
+        Find mentoring and recently-upgraded blocks. We check the recently upgraded ones
+        in case an error happened and we're re-running the upgrade.
+        """
+        # If it's a v1 block or a recently upgraded block it will have xml_content:
+        if isinstance(block, (MentoringBlock, NewMentoringBlock)) and block.xml_content:
             blocks_found.append(block.scope_ids.usage_id)
         elif block.has_children:
             for child_id in block.children:
                 find_mentoring_blocks(block.runtime.get_block(child_id))
     find_mentoring_blocks(course)
+
     total = len(blocks_found)
     print(" ➔ Found {} mentoring blocks".format(total))
+
+    print(" ➔ Doing a quick sanity check of the url_names")
+    url_names = set()
+    stop = False
+    for block_id in blocks_found:
+        block = course.runtime.get_block(block_id)
+        if block.url_name in url_names:
+            print(u" ➔ Mentoring block {} conflicts with another block using the same url_name".format(block.url_name))
+            print(u'   (display_name: "{}", parent {}: "{}")'.format(
+                block.display_name, block.parent, block.get_parent().display_name
+            ))
+            stop = True
+            continue
+        if block.url_name != unicode(block_id.block_id):
+            print(u" ➔ Mentoring block {} has two different url_names assigned. Real url_name is {}".format(
+                unicode(block_id.block_id)
+            ))
+            print(u'   (display_name: "{}", parent: "{}")'.format(block.display_name, block.get_parent().display_name))
+            print(u"   (to fix, edit the XML and set the url_name to the real value given above).")
+            stop = True
+        url_names.add(block.url_name)
+
+    if stop:
+        print(" ➔ Exiting due to errors preventing the upgrade.")
+        sys.exit(1)
 
     with store.bulk_operations(course.location.course_key):
         count = 1
