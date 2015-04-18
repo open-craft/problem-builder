cb4538c91f09d2caadc26d84f13feed533236f18
Braden MacDonald <braden@opencraft.com>
2015-04-17 18:12:15 -0700

    Fix bug when multiple mentoring blocks are in the same unit, one would disappear on upgrade

--- problem_builder/v1/upgrade.py
+++ problem_builder/v1/upgrade.py
@@ -76,7 +76,7 @@ def upgrade_block(block):
     root.attrib["xml_content"] = xml_content_str
 
     # Was block already published?
-    parent = block.runtime.get_block(block.parent)  # Don't use get_parent() as it may be an outdated cached version
+    parent = store.get_item(block.parent)  # Don't use get_parent()/get_block() as it may be an outdated cached version
     parent_was_published = not store.has_changes(parent)
 
     old_usage_id = block.location
