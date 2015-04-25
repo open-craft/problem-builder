06b7460b12ee544d709be9af131d6605a4a54885
Braden MacDonald <braden@opencraft.com>
2015-04-24 19:17:53 -0700

    Submissions API demonstration - completes the proof of concept

--- problem_builder/tasks.py
+++ problem_builder/tasks.py
@@ -9,6 +9,7 @@ from opaque_keys.edx.keys import UsageKey
 from xmodule.modulestore.django import modulestore
 
 from .mcq import MCQBlock, RatingBlock
+from .sub_api import sub_api
 
 logger = get_task_logger(__name__)
 
@@ -24,7 +25,8 @@ def export_data(source_block_id_str, user_id):
 
     block_key = UsageKey.from_string(source_block_id_str)
     src_block = modulestore().get_item(block_key)
-    course_key = src_block.scope_ids.usage_id.course_key
+    course_key = src_block.scope_ids.usage_id.course_key.replace(branch=None, version_guid=None)
+    course_key_str = unicode(course_key)
 
     # Get the root block:
     root = src_block
@@ -46,12 +48,26 @@ def export_data(source_block_id_str, user_id):
 
     # Define the header rows of our CSV:
     rows = []
-    rows.append(block.display_name_with_default for block in blocks_to_include)
-    rows.append(block.scope_ids.block_type for block in blocks_to_include)
-    rows.append(block.scope_ids.usage_id for block in blocks_to_include)
+    rows.append(["Student"] + [block.display_name_with_default for block in blocks_to_include])
+    rows.append([""] + [block.scope_ids.block_type for block in blocks_to_include])
+    rows.append([""] + [block.scope_ids.usage_id for block in blocks_to_include])
 
     # Load the actual student submissions for each block in blocks_to_include.
     # Note this requires one giant query per block (all student submissions for each block, one block at a time)
+    student_submissions = {}  # Key is student ID, value is a list with same length as blocks_to_include
+    for idx, block in enumerate(blocks_to_include, start=1):  # start=1 since first column is stuent ID
+        # Get all of the most recent student submissions for this block:
+        block_id = unicode(block.scope_ids.usage_id.replace(branch=None, version_guid=None))
+        block_type = block.scope_ids.block_type
+        for submission in sub_api.get_all_submissions(course_key_str, block_id, block_type):
+            if submission.student_id not in student_submissions:
+                student_submissions[submission.student_id] = [submission.student_id] + [""] * len(blocks_to_include)
+            student_submissions[submission.student_id][idx] = submission.answer
+
+    # Now change from a dict to an array ordered by student ID as we generate the remaining rows:
+    for student_id in sorted(student_submissions.iterkeys()):
+        rows.append(student_submissions[student_id])
+        del student_submissions[student_id]
 
     # Generate the CSV:
     filename = u"pb-data-export-{}.csv".format(report_date.strftime("%Y-%m-%d-%H%M%S"))
