60713fddbb0db22ffccfd8706e1f005f6bc31795
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-05-05 15:29:21 +0300

    Accessibility updates - using sr and aria-hidden for better screen reader experience

--- problem_builder/dashboard.py
+++ problem_builder/dashboard.py
@@ -402,6 +402,7 @@ class DashboardBlock(StudioEditableXBlockMixin, XBlock):
                 block['mcqs'].append({
                     "display_name": mcq_block.display_name_with_default,
                     "value": value,
+                    "accessible_value": _("Score: {value}").format(value=value) if value else _("No value yet"),
                     "color": self.color_for_value(value) if value is not None else None,
                 })
             # If the values are numeric, display an average:
@@ -412,6 +413,8 @@ class DashboardBlock(StudioEditableXBlockMixin, XBlock):
             if numeric_values:
                 average_value = sum(numeric_values) / len(numeric_values)
                 block['average'] = average_value
+                # average block is shown only if average value exists, so accessible text for no data is not required
+                block['accessible_average'] = _("Score: {average_value:.2f}").format(average_value=average_value)
                 block['average_label'] = self.average_labels.get(mentoring_block.url_name, _("Average"))
                 block['has_average'] = True
                 block['average_color'] = self.color_for_value(average_value)
