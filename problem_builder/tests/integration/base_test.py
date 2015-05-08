bfd829e97652015168f608717a8af29af5e47724
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-05-08 15:04:18 +0300

    Added tests for initial state (no feedback should be shown) and for perfect-score-in-the-past scenario

--- problem_builder/tests/integration/base_test.py
+++ problem_builder/tests/integration/base_test.py
@@ -92,6 +92,13 @@ class ProblemBuilderBaseTest(SeleniumXBlockTest, PopupCheckMixin):
                 label.click()
                 break
 
+    def click_choice(self, container, choice_text):
+        """ Click on the choice label with the specified text """
+        for label in container.find_elements_by_css_selector('.choice label'):
+            if choice_text in label.text:
+                label.click()
+                break
+
 
 class MentoringBaseTest(SeleniumBaseTest, PopupCheckMixin):
     module_name = __name__
