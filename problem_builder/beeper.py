5d54468046a5d6700973b96b88407ec19018d6af
Braden MacDonald <braden@opencraft.com>
2015-06-12 14:22:06 -0700

    Debugging

--- problem_builder/beeper.py
+++ problem_builder/beeper.py
@@ -22,8 +22,15 @@ class BeeperBlock(StudioEditableXBlockMixin, XBlock):
     def student_view(self, context):
         normalized_id = self.scope_ids.usage_id.replace(run=None, branch=None, version_guid=None)
         fragment = Fragment()
-        if context.get('activate_block_id', None) == normalized_id:
+        fragment.add_content(u'<div><h4>Beeper</h4>')
+        fragment.add_content(u'<p>normalized_id: {}</p>'.format(unicode(normalized_id)))
+        fragment.add_content(u'<p>activated: {}</p>'.format(context.get('activate_block_id', '')))
+        if context.get('activate_block_id', None) == unicode(normalized_id):
             fragment.add_javascript("alert('{content}')".format(content=self.content))
+            fragment.add_content(u'<p>Activated!</p>')
+        else:
+            fragment.add_content(u'<p>Not Activated.</p>')
+        fragment.add_content(u'</div>')
         return fragment
 
     def author_view(self, content):
