b85556701b717157dffdb8da052753000245fd81
Matjaz Gregoric <mtyaka@gmail.com>
2016-11-23 09:18:09 +0100

    Introduce Answer.course_key and deprecate course_id.
    This introduces a new 255-char course_key field to the Answer model.
    The old 50-char course_id field is still present, but is deprecated and
    will be removed in next release.
    
    We cannot simply extend the existing course_id field because we have
    some large problem_builder_answer tables in production and the migration
    to extend the column would lock the table causing issues in production.
    
    The code is updated so that it uses the new course_key column, but falls
    back to course_id.
    In next release, a data migration to copy course_id data to course_key
    will be provided and the course_id column dropped.

---
+++ problem_builder/migrations/0003_auto_20161124_0755.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('problem_builder', '0002_auto_20160121_1525'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='answer',
+            name='course_key',
+            field=models.CharField(default=None, max_length=255, null=True, db_index=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='answer',
+            unique_together=set([('student_id', 'course_key', 'name'), ('student_id', 'course_id', 'name')]),
+        ),
+    ]
--- problem_builder/models.py
+++ problem_builder/models.py
@@ -35,11 +35,19 @@ class Answer(models.Model):
     """
 
     class Meta:
-        unique_together = (('student_id', 'course_id', 'name'),)
+        unique_together = (
+            ('student_id', 'course_id', 'name'),
+            ('student_id', 'course_key', 'name'),
+        )
 
     name = models.CharField(max_length=50, db_index=True)
     student_id = models.CharField(max_length=32, db_index=True)
+    # course_id is deprecated; it will be removed in next release.
     course_id = models.CharField(max_length=50, db_index=True)
+    # course_key is the new course_id replacement with extended max_length.
+    # We need to allow NULL values during the transition period,
+    # but we will remove the null=True and default=None parameters in next release.
+    course_key = models.CharField(max_length=255, db_index=True, null=True, default=None)
     student_input = models.TextField(blank=True, default='')
     created_on = models.DateTimeField('created on', auto_now_add=True)
     modified_on = models.DateTimeField('modified on', auto_now=True)
