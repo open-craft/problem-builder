f94d3663afd289cdd763128b16ea425cf63724f9
Tim Krones <tim@opencraft.com>
2017-01-24 17:35:09 +0100

    MRQ: Don't error out if there are no results for current user.

--- problem_builder/public/js/questionnaire.js
+++ problem_builder/public/js/questionnaire.js
@@ -221,7 +221,9 @@ function MRQBlock(runtime, element) {
                 display_message(result.message, messageView, options.checkmark);
             }
 
-            $.each(result.choices, function(index, choice) {
+            // If user has never submitted an answer for this MRQ, `result` will be empty.
+            // So we need to fall back on an empty list for `result.choices` to avoid errors in the next step:
+            $.each(result.choices || [], function(index, choice) {
                 var choiceInputDOM = $('.choice input[value='+choice.value+']', element);
                 var choiceDOM = choiceInputDOM.closest('.choice');
                 var choiceResultDOM = $('.choice-result', choiceDOM);
