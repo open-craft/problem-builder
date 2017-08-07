750ad3658ac9de67b69583bdff795c5bff843eec
Brad Melin <melinbrad@gmail.com>
2017-08-07 10:17:53 +0200

    Bump version to 2.6.10

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ BLOCKS = [
 
 setup(
     name='xblock-problem-builder',
-    version='2.6.9',
+    version='2.6.10',
     description='XBlock - Problem Builder',
     packages=['problem_builder', 'problem_builder.v1'],
     install_requires=[
