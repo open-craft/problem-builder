f76937548763485969e7cef0134644ef59610a1f
J. Cliff Dyer <jcd@sdf.org>
2017-09-19 09:48:57 -0400

    Remove course_id from answer model.
    No migration added, per request from @edx/devops
    
    OC-3033

--- problem_builder/models.py
+++ problem_builder/models.py
@@ -39,15 +39,11 @@ class Answer(models.Model):
         # specify the app_label here.
         app_label = 'problem_builder'
         unique_together = (
-            ('student_id', 'course_id', 'name'),
             ('student_id', 'course_key', 'name'),
         )
 
     name = models.CharField(max_length=50, db_index=True)
     student_id = models.CharField(max_length=32, db_index=True)
-    # course_id is deprecated; it will be removed in next release.
-    course_id = models.CharField(max_length=50, db_index=True, blank=True, null=True, default=None)
-    # course_key is the new course_id replacement with extended max_length.
     course_key = models.CharField(max_length=255, db_index=True)
     student_input = models.TextField(blank=True, default='')
     created_on = models.DateTimeField('created on', auto_now_add=True)
