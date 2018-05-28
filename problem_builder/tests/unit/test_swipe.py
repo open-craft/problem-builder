bd4770d8e27c2d48d6d1f61505a13c660a917ef2
Braden MacDonald <braden@opencraft.com>
2018-05-28 11:21:10 -0400

    Fix: default 'correctness' of swipe is null, not false

---
+++ problem_builder/tests/unit/test_swipe.py
@@ -0,0 +1,19 @@
+import ddt
+import unittest
+from mock import Mock
+
+from xblock.field_data import DictFieldData
+from problem_builder.swipe import SwipeBlock
+
+
+@ddt.ddt
+class TestSwipeBlock(unittest.TestCase):
+    def test_student_view_data(self):
+        """
+        Ensure that all expected fields are always returned with
+        appropriate values.
+        """
+        runtime = Mock(replace_urls=lambda url: '"{}"'.format(url))
+        block = SwipeBlock(runtime, DictFieldData({"display_name": "Test"}), Mock())
+
+        self.assertEqual(block.student_view_data()['correct'], False)
