5b1c082b5a8a400cccfe906368f0ea907556b9fb
Braden MacDonald <braden@opencraft.com>
2018-05-28 11:21:10 -0400

    Fix: default 'correctness' of swipe is null, not false

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ BLOCKS = [
 
 setup(
     name='xblock-problem-builder',
-    version='3.0.0',
+    version='3.0.1',
     description='XBlock - Problem Builder',
     packages=find_packages(),
     install_requires=[
