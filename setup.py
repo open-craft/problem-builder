09de07df2a1cad23ddbe083dc2bea93110afc26e
Jillian Vogel <jill@opencraft.com>
2018-05-31 14:21:27 -0400

    Removes custom apros.css (#192)
    * Removes custom apros.css
    * Bumps version to 3.1.0

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ BLOCKS = [
 
 setup(
     name='xblock-problem-builder',
-    version='3.1.0',
+    version='3.1.1',
     description='XBlock - Problem Builder',
     packages=find_packages(),
     install_requires=[
