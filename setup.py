5024b2291ada255e952dc28fc75b45e423facef2
Jillian Vogel <jill@opencraft.com>
2018-06-07 10:39:13 -0400

    Remove all reference to ThemableXBlockMixin (#195)
    * Remove all reference to ThemableXBlockMixin and its methods
    * Move the lms.css theme file and include it manually in the LMS view(s)
    * Bumps version to 3.1.2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ BLOCKS = [
 
 setup(
     name='xblock-problem-builder',
-    version='3.1.1',
+    version='3.1.2',
     description='XBlock - Problem Builder',
     packages=find_packages(),
     install_requires=[
