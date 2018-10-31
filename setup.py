e42a287e5e209e258a8760217dbe6e5d2d3a2b75
J. Cliff Dyer <jcd@sdf.org>
2018-10-31 13:11:19 -0400

    Version bump to 2.11

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ BLOCKS = [
 
 setup(
     name='xblock-problem-builder',
-    version='2.10.5',
+    version='2.11.0',
     description='XBlock - Problem Builder',
     packages=find_packages(),
     install_requires=[
