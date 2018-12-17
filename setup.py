4c2b811d9f6990215257c4cdfd0756b2755cb63c
Agrendalath <piotr@surowiec.it>
2018-12-17 14:19:55 +0100

    [MCKIN-8350] Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.1.5'
+VERSION = '3.1.6'
 
 
 # Functions #########################################################
