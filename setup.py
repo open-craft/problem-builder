41bb862421b53f37ecb5c513f70c4f0b094a2730
Kshitij Sobti <kshitij@sobti.in>
2019-06-21 09:14:56 +0530

    Add version bump

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.3.7'
+VERSION = '3.3.8'
 
 # Functions #########################################################
 
