76563aa7d267f5c6b136b7988ff6800f46d7c36e
Muhammad Usman <m.usman@arbisoft.com>
2019-11-13 23:34:56 +0500

    version updated

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.3.9'
+VERSION = '3.3.12'
 
 # Functions #########################################################
 
