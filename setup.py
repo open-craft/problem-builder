91e85293e2f7507a54e1c1a6cc3b11496b878ca3
Nasir Hussain <nasirhjafri@gmail.com>
2020-01-03 08:58:45 +0500

    MCKIN-12576 Pause videos on reviewNextStep (#255)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.4.3'
+VERSION = '3.4.4'
 
 # Functions #########################################################
 
