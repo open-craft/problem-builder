bdef3cf5527e3736ee64117a818ba38d1f3a44c7
moeez96 <moeezzahid1996@gmail.com>
2020-02-28 12:38:45 +0500

    MCKIN-13390 JA & AR translation fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.4.10'
+VERSION = '3.4.11'
 
 # Functions #########################################################
 
