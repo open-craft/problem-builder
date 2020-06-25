047b20908f24bc55b26c6cac2be6ab36c0387076
Saqib <msaqib52@gmail.com>
2020-06-25 07:14:53 +0500

    MCKIN-19683: fix translations issues (#288) (#289)
    * fix translations issues
    
    * korean translation update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '4.0.3'
+VERSION = '4.0.4'
 
 # Functions #########################################################
 
