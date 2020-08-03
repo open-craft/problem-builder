e9883bb38b32b3f4d6498d9aa884d05a6b563d26
Muhammad Usman <43761905+musmanmalik@users.noreply.github.com>
2020-08-03 19:01:35 +0500

    Korean Translation Update (#292)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.4.21'
+VERSION = '3.4.22'
 
 # Functions #########################################################
 
