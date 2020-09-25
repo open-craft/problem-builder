3c2e9a36fefc1ed7684dc2c607df74f5d8c571a3
Wasif ur Rehman <wasif.rehman@arbisoft.com>
2020-09-25 16:09:51 +0500

    MCKIN-24785. Platform issues - Instructor tool xblock strings for translations

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.5.2'
+VERSION = '3.5.4'
 
 # Functions #########################################################
 
