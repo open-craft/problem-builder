5f5c81c4ce9989f9da0b30988599ee0ff8a1f0f1
Wasif ur Rehman <wasifarbisoft@users.noreply.github.com>
2020-09-28 10:56:17 +0500

    MCKIN-24785. Platform issues - Instructor tool xblock strings for translations (#307)
    https://edx-wiki.atlassian.net/browse/MCKIN-24785

--- problem_builder/mixins.py
+++ problem_builder/mixins.py
@@ -5,6 +5,7 @@ import six
 import webob
 from django import utils
 from lazy import lazy
+from .settings import PB_LANGUAGE_JS_DIRECTORY_MAP
 from xblock.core import XBlock
 from xblock.fields import UNIQUE_ID, Boolean, Float, Scope, String
 from xblock.fragment import Fragment
@@ -306,13 +307,12 @@ class TranslationContentMixin(object):
 
     def get_translation_content(self):
         try:
-            # here we need to split the lang code and need to change - to _ and post - characters to
-            # upper case since we have local directories like ja_JP, etc instead of ja-jp, etc
-            language = utils.translation.get_language().split('-')
-            if len(language) == 2:
-                new_lang = language[0] + "_" + language[1].upper()
-            else:
-                new_lang = utils.translation.get_language()
-            return self.resource_string('public/js/translations/{lang}/textjs.js'.format(lang=new_lang))
+            language = utils.translation.get_language()
+            # the language code for some languages are not consistent
+            # with the js directory structure of problem builder
+            # i.e for polish language code is pl but js directory is pl_PL
+            # to coup this behaviour PB_LANGUAGES_JS_DIRECTORY_MAP is added to get the desired directory.
+            js_directory = PB_LANGUAGE_JS_DIRECTORY_MAP.get(language, 'en')
+            return self.resource_string('public/js/translations/{lang}/textjs.js'.format(lang=js_directory))
         except IOError:
             return self.resource_string('public/js/translations/en/textjs.js')
