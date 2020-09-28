6014f24c9d7b99077541081c142583d2767ce60f
musmanmalik <m.usman@arbisoft.com>
2020-09-28 17:06:24 +0500

    Translation correction

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '4.1.4'
+VERSION = '4.1.5'
 
 # Functions #########################################################
 
