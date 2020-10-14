d49facf428c56d22323deeb60f158ceee1a78120
Wasif ur Rehman <wasif.rehman@arbisoft.com>
2020-10-14 17:01:26 +0500

    MCKIN-26123 Platform Issues - Translations.

--- problem_builder/public/js/translations/fr/textjs.js
+++ problem_builder/public/js/translations/fr/textjs.js
@@ -2,13 +2,13 @@
             (function(global){
                 var ProblemBuilderXBlockI18N = {
                   init: function() {
-                    
+
 
 (function(globals) {
 
   var django = globals.django || (globals.django = {});
 
-  
+
   django.pluralidx = function(n) {
     var v=(n > 1);
     if (typeof(v) == 'boolean') {
@@ -17,25 +17,29 @@
       return v;
     }
   };
-  
+
 
   /* gettext library */
 
   django.catalog = django.catalog || {};
-  
+
   var newcatalog = {
-    "All": "Tout", 
-    "Data export failed. Reason: <%= error %>": "\u00c9chec d'exportation des donn\u00e9es. Motif\u00a0: <%= error %>", 
+    "All": "Tout",
+    "Data export failed. Reason: <%= error %>": "\u00c9chec d'exportation des donn\u00e9es. Motif\u00a0: <%= error %>",
     "Results retrieved on <%= creation_time %> (<%= seconds %> second).": [
-      "R\u00e9sultats r\u00e9cup\u00e9r\u00e9s le <%= creation_time %> (<%= seconds %> seconde).", 
+      "R\u00e9sultats r\u00e9cup\u00e9r\u00e9s le <%= creation_time %> (<%= seconds %> seconde).",
       "R\u00e9sultats r\u00e9cup\u00e9r\u00e9s le <%= creation_time %> (<%= seconds %> secondes)."
-    ], 
-    "The report is currently being generated\u2026": "Le rapport est en cours de production..."
+    ],
+    "The report is currently being generated\u2026": "Le rapport est en cours de production...",
+    "You have used {num_used} of 1 submission.": [
+      "Vous avez utilis\u00e9 {num_used} sur 1 envoi.",
+      "Vous avez utilis\u00e9 {num_used} sur {max_attempts} envois."
+    ]
   };
   for (var key in newcatalog) {
     django.catalog[key] = newcatalog[key];
   }
-  
+
 
   if (!django.jsi18n_initialized) {
     django.gettext = function(msgid) {
@@ -86,42 +90,42 @@
     /* formatting library */
 
     django.formats = {
-    "DATETIME_FORMAT": "j F Y H:i", 
+    "DATETIME_FORMAT": "j F Y H:i",
     "DATETIME_INPUT_FORMATS": [
-      "%d/%m/%Y %H:%M:%S", 
-      "%d/%m/%Y %H:%M:%S.%f", 
-      "%d/%m/%Y %H:%M", 
-      "%d/%m/%Y", 
-      "%d.%m.%Y %H:%M:%S", 
-      "%d.%m.%Y %H:%M:%S.%f", 
-      "%d.%m.%Y %H:%M", 
-      "%d.%m.%Y", 
-      "%Y-%m-%d %H:%M:%S", 
-      "%Y-%m-%d %H:%M:%S.%f", 
-      "%Y-%m-%d %H:%M", 
+      "%d/%m/%Y %H:%M:%S",
+      "%d/%m/%Y %H:%M:%S.%f",
+      "%d/%m/%Y %H:%M",
+      "%d/%m/%Y",
+      "%d.%m.%Y %H:%M:%S",
+      "%d.%m.%Y %H:%M:%S.%f",
+      "%d.%m.%Y %H:%M",
+      "%d.%m.%Y",
+      "%Y-%m-%d %H:%M:%S",
+      "%Y-%m-%d %H:%M:%S.%f",
+      "%Y-%m-%d %H:%M",
       "%Y-%m-%d"
-    ], 
-    "DATE_FORMAT": "j F Y", 
+    ],
+    "DATE_FORMAT": "j F Y",
     "DATE_INPUT_FORMATS": [
-      "%d/%m/%Y", 
-      "%d/%m/%y", 
-      "%d.%m.%Y", 
-      "%d.%m.%y", 
+      "%d/%m/%Y",
+      "%d/%m/%y",
+      "%d.%m.%Y",
+      "%d.%m.%y",
       "%Y-%m-%d"
-    ], 
-    "DECIMAL_SEPARATOR": ",", 
-    "FIRST_DAY_OF_WEEK": "1", 
-    "MONTH_DAY_FORMAT": "j F", 
-    "NUMBER_GROUPING": "3", 
-    "SHORT_DATETIME_FORMAT": "j N Y H:i", 
-    "SHORT_DATE_FORMAT": "j N Y", 
-    "THOUSAND_SEPARATOR": "\u00a0", 
-    "TIME_FORMAT": "H:i", 
+    ],
+    "DECIMAL_SEPARATOR": ",",
+    "FIRST_DAY_OF_WEEK": "1",
+    "MONTH_DAY_FORMAT": "j F",
+    "NUMBER_GROUPING": "3",
+    "SHORT_DATETIME_FORMAT": "j N Y H:i",
+    "SHORT_DATE_FORMAT": "j N Y",
+    "THOUSAND_SEPARATOR": "\u00a0",
+    "TIME_FORMAT": "H:i",
     "TIME_INPUT_FORMATS": [
-      "%H:%M:%S", 
-      "%H:%M:%S.%f", 
+      "%H:%M:%S",
+      "%H:%M:%S.%f",
       "%H:%M"
-    ], 
+    ],
     "YEAR_MONTH_FORMAT": "F Y"
   };
 
@@ -155,4 +159,3 @@
                 ProblemBuilderXBlockI18N.init();
                 global.ProblemBuilderXBlockI18N = ProblemBuilderXBlockI18N;
             }(this));
-        
--- problem_builder/public/js/translations/zh_CN/textjs.js
+++ problem_builder/public/js/translations/zh_CN/textjs.js
@@ -2,44 +2,44 @@
             (function(global){
                 var ProblemBuilderXBlockI18N = {
                   init: function() {
-                    
+
 
 (function(globals) {
 
   var django = globals.django || (globals.django = {});
 
-  
+
   django.pluralidx = function(n) {
-    var v=0;
+    var v=(n != 1);
     if (typeof(v) == 'boolean') {
       return v ? 1 : 0;
     } else {
       return v;
     }
   };
-  
+
 
   /* gettext library */
 
   django.catalog = django.catalog || {};
-  
+
   var newcatalog = {
-    "All": "\u5168\u90e8", 
-    "Data export failed. Reason: <%= error %>": "\u6570\u636e\u5bfc\u51fa\u5931\u8d25\u3002\u539f\u56e0\uff1a<%= error %>", 
+    "All": "\u5168\u90e8",
+    "Data export failed. Reason: <%= error %>": "\u6570\u636e\u5bfc\u51fa\u5931\u8d25\u3002\u539f\u56e0\uff1a<%= error %>",
     "Results retrieved on <%= creation_time %> (<%= seconds %> second).": [
-      "<%= creation_time %> (<%= seconds %> \u79d2) \u65f6\u68c0\u7d22\u5230\u7684\u7ed3\u679c\u3002", 
+      "<%= creation_time %> (<%= seconds %> \u79d2) \u65f6\u68c0\u7d22\u5230\u7684\u7ed3\u679c\u3002",
       "<%= creation_time %> (<%= seconds %> \u79d2) \u65f6\u68c0\u7d22\u5230\u7684\u7ed3\u679c\u3002"
-    ], 
-    "The report is currently being generated\u2026": "\u76ee\u524d\u6b63\u5728\u751f\u6210\u62a5\u544a\u2026", 
+    ],
+    "The report is currently being generated\u2026": "\u76ee\u524d\u6b63\u5728\u751f\u6210\u62a5\u544a\u2026",
     "You have used {num_used} of 1 submission.": [
-      "\u60a8\u5df2\u4f7f\u7528 {num_used} \u6b21\u63d0\u4ea4\uff08\u5171 1 \u6b21\uff09\u3002", 
+      "\u60a8\u5df2\u4f7f\u7528 {num_used} \u6b21\u63d0\u4ea4\uff08\u5171 1 \u6b21\uff09\u3002",
       "\u60a8\u5df2\u4f7f\u7528 {num_used} \u6b21\u63d0\u4ea4\uff08\u5171 {max_attempts} \u6b21\uff09"
     ]
   };
   for (var key in newcatalog) {
     django.catalog[key] = newcatalog[key];
   }
-  
+
 
   if (!django.jsi18n_initialized) {
     django.gettext = function(msgid) {
@@ -90,48 +90,48 @@
     /* formatting library */
 
     django.formats = {
-    "DATETIME_FORMAT": "N j, Y, P", 
+    "DATETIME_FORMAT": "N j, Y, P",
     "DATETIME_INPUT_FORMATS": [
-      "%Y-%m-%d %H:%M:%S", 
-      "%Y-%m-%d %H:%M:%S.%f", 
-      "%Y-%m-%d %H:%M", 
-      "%Y-%m-%d", 
-      "%m/%d/%Y %H:%M:%S", 
-      "%m/%d/%Y %H:%M:%S.%f", 
-      "%m/%d/%Y %H:%M", 
-      "%m/%d/%Y", 
-      "%m/%d/%y %H:%M:%S", 
-      "%m/%d/%y %H:%M:%S.%f", 
-      "%m/%d/%y %H:%M", 
+      "%Y-%m-%d %H:%M:%S",
+      "%Y-%m-%d %H:%M:%S.%f",
+      "%Y-%m-%d %H:%M",
+      "%Y-%m-%d",
+      "%m/%d/%Y %H:%M:%S",
+      "%m/%d/%Y %H:%M:%S.%f",
+      "%m/%d/%Y %H:%M",
+      "%m/%d/%Y",
+      "%m/%d/%y %H:%M:%S",
+      "%m/%d/%y %H:%M:%S.%f",
+      "%m/%d/%y %H:%M",
       "%m/%d/%y"
-    ], 
-    "DATE_FORMAT": "N j, Y", 
+    ],
+    "DATE_FORMAT": "N j, Y",
     "DATE_INPUT_FORMATS": [
-      "%Y-%m-%d", 
-      "%m/%d/%Y", 
-      "%m/%d/%y", 
-      "%b %d %Y", 
-      "%b %d, %Y", 
-      "%d %b %Y", 
-      "%d %b, %Y", 
-      "%B %d %Y", 
-      "%B %d, %Y", 
-      "%d %B %Y", 
+      "%Y-%m-%d",
+      "%m/%d/%Y",
+      "%m/%d/%y",
+      "%b %d %Y",
+      "%b %d, %Y",
+      "%d %b %Y",
+      "%d %b, %Y",
+      "%B %d %Y",
+      "%B %d, %Y",
+      "%d %B %Y",
       "%d %B, %Y"
-    ], 
-    "DECIMAL_SEPARATOR": ".", 
-    "FIRST_DAY_OF_WEEK": "0", 
-    "MONTH_DAY_FORMAT": "F j", 
-    "NUMBER_GROUPING": "0", 
-    "SHORT_DATETIME_FORMAT": "m/d/Y P", 
-    "SHORT_DATE_FORMAT": "m/d/Y", 
-    "THOUSAND_SEPARATOR": ",", 
-    "TIME_FORMAT": "P", 
+    ],
+    "DECIMAL_SEPARATOR": ".",
+    "FIRST_DAY_OF_WEEK": "0",
+    "MONTH_DAY_FORMAT": "F j",
+    "NUMBER_GROUPING": "0",
+    "SHORT_DATETIME_FORMAT": "m/d/Y P",
+    "SHORT_DATE_FORMAT": "m/d/Y",
+    "THOUSAND_SEPARATOR": ",",
+    "TIME_FORMAT": "P",
     "TIME_INPUT_FORMATS": [
-      "%H:%M:%S", 
-      "%H:%M:%S.%f", 
+      "%H:%M:%S",
+      "%H:%M:%S.%f",
       "%H:%M"
-    ], 
+    ],
     "YEAR_MONTH_FORMAT": "F Y"
   };
 
@@ -165,4 +165,3 @@
                 ProblemBuilderXBlockI18N.init();
                 global.ProblemBuilderXBlockI18N = ProblemBuilderXBlockI18N;
             }(this));
-        
