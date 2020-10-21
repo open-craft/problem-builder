0726850a142a88d90ecd8db4a06c05125c9295df
Wasif ur Rehman <wasifarbisoft@users.noreply.github.com>
2020-10-21 23:39:12 +0500

    MCKIN-26505  Arabic -> Step Builder -> Score Summary -> The translations are not correct if the none of the questions are right/partially right/incorrect (#317)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '3.5.6'
+VERSION = '3.5.7'
 
 # Functions #########################################################
 
