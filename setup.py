e400de11f60a69a17bdcacee6e0bab23f04fbfc5
Mudassir Hafeez <mudasir5761@gmail.com>
2021-04-22 15:41:39 +0500

    version bump to 4.1.13

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '4.1.12'
+VERSION = '4.1.13'
 
 # Functions #########################################################
 
